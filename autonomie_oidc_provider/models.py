66a0cc2d593526390ffb8e7a6a3596d3cf0be6fd
G.Tjebbes <g.t@majerti.fr>
2017-07-26 14:46:50 +0200

    Use gen_salt

--- autonomie_oidc_provider/models.py
+++ autonomie_oidc_provider/models.py
@@ -3,9 +3,8 @@
 #       * TJEBBES Gaston <g.t@majerti.fr>
 #       * Arezki Feth <f.a@majerti.fr>;
 #       * Miotte Julien <j.m@majerti.fr>;
-
-import time
-from datetime import datetime
+import jwt
+import datetime
 
 from sqlalchemy import Column
 from sqlalchemy import ForeignKey
@@ -18,19 +17,56 @@ from sqlalchemy import Unicode
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import synonym
 
-from .util import oidc_settings
+from .util import (
+    oidc_settings,
+    dt_to_timestamp,
+)
 
 from .generators import (
     gen_token,
     gen_client_id,
     gen_client_secret,
     crypt_secret,
+    gen_salt,
 )
 
-from autonomie.models.base import (
+from autonomie_base.models.base import (
     DBBASE,
     default_table_args,
+    DBSESSION,
 )
+
+
+SECRET = "Test"
+
+
+def get_client_by_client_id(client_id, valid=True):
+    """
+    Return The client matching the given client_id
+
+    :param str client_id: The client_id to look for
+    :param bool valid: Only non-revoked clients ?
+    :returns: A OidcClient instance (default None)
+    :rtype: obj
+    """
+    query = OidcClient.query().filter_by(client_id=client_id)
+    if valid:
+        query = query.filter_by(revoked=False)
+    return query.first()
+
+
+def get_code_by_client_id(client_id, code):
+    """
+    Find a code issued by this Provider
+
+    :param str client_id: The client_id to look for
+    :param str code: The code to check
+    :returns: The OidcCode instance
+    :rtype: obj
+    """
+    query = OidcCode.query().filter_by(client_id=client_id)
+    query = query.filter_by(authcode=code)
+    return query.first()
 
 
 class OidcClient(DBBASE):
@@ -38,10 +74,15 @@ class OidcClient(DBBASE):
     id = Column(Integer, primary_key=True)
     name = Column(Unicode(128), unique=True, nullable=False)
     client_id = Column(Unicode(64), unique=True, nullable=False)
-    _client_secret = Column(Unicode(255), unique=True, nullable=False)
+    _client_secret = Column(
+        Unicode(255),
+        unique=True, nullable=False)
     revoked = Column(Boolean, default=False)
     revocation_date = Column(DateTime)
-    _salt = None
+    scopes = Column(Unicode(256), nullable=False)
+
+    salt = Column(Unicode(256), nullable=False)
+    cert_salt = Column(Unicode(256), default='')
     redirect_uris = relationship(
         "OidcRedirectUri",
         back_populates='client',
@@ -49,11 +90,14 @@ class OidcClient(DBBASE):
     tokens = relationship("OidcToken", back_populates="client")
     authcodes = relationship("OidcCode", back_populates="client")
 
-    def __init__(self, name, salt=None):
+    def __init__(self, name, scopes, cert_salt=None):
         self.name = name
-        self._salt = salt
+        self.salt = gen_salt()
         self.client_id = gen_client_id()
         self.client_secret = gen_client_secret()
+        self.scopes = scopes
+        if cert_salt:
+            self.cert_salt = cert_salt
 
     def new_client_secret(self):
         """
@@ -67,53 +111,71 @@ class OidcClient(DBBASE):
         return secret
 
     def _get_client_secret(self):
+        """
+        Getter for the current client secret
+
+        :returns: The client secret value
+        :rtype: str
+        """
         return self._client_secret
 
-    def _validate_salt(self, salt):
+    def _crypt_secret(self, client_secret):
         """
-        Check that the given salt could be b64decoded
+        Returns a crypted version of the client_secret
 
-        :param str salt: The salt to check
-        :raises: ValueError
+        :param unicode client_secret: The clear client secret
+        :returns: A crypted client secret
+        :rtype: unicode
         """
-        salt_len = len(salt)
-        if salt_len == 0:
-            raise ValueError(
-                "Missing an oidc.salt setting "
-                "(it's length should be a multiple of 4)"
-            )
-        if salt_len % 4 != 0:
-            raise ValueError(
-                "oidc.salt configuration length should be a multiple of 4"
-            )
+        if isinstance(client_secret, unicode):
+            client_secret = client_secret.encode('utf-8')
+        client_secret = bytes(client_secret)
+        return crypt_secret(client_secret, self.salt)
 
     def _set_client_secret(self, client_secret):
         """
         Define the client secret
-        1- extract the salt from the current configuration
-        2- validates the salt format
-        3-
         """
-        if self._salt is None:
-            self._salt = oidc_settings('salt', '')
-
-        self._validate_salt(self._salt)
-
-        if isinstance(client_secret, unicode):
-            client_secret = client_secret.encode('utf-8')
-        client_secret = bytes(client_secret)
-
-        self._client_secret = crypt_secret(client_secret, self._salt)
+        self._client_secret = self._crypt_secret(client_secret)
 
     client_secret = synonym('_client_secret', descriptor=property(
         _get_client_secret, _set_client_secret))
 
     def revoke(self):
+        """
+        Revoke the given client
+        """
         self.revoked = True
-        self.revocation_date = datetime.utcnow()
+        self.revocation_date = datetime.datetime.utcnow()
 
-    def isRevoked(self):
+    def is_revoked(self):
+        """
+        Check if the current client is still valid
+        """
         return self.revoked
+
+    def check_secret(self, client_secret):
+        """
+        Check that the given secret matches the current one
+
+        :param str client_secret: The client secret transmitted by the Resource
+        Consumer
+        :param str salt: The salt used for encryption (see configuration)
+        """
+        encrypted = self._crypt_secret(client_secret)
+        return encrypted == self.client_secret
+
+    def check_scope(self, scopes):
+        """
+        Check that the scopes are allowed for the current client
+
+        :param list scope: list of requested scopes
+        :returns: True / False
+        :rtype: bool
+        """
+        scopes = set(scopes)
+        allowed_scopes = set(self.scopes.split(' '))
+        return scopes.issubset(allowed_scopes)
 
 
 class OidcRedirectUri(DBBASE):
@@ -136,30 +198,46 @@ class OidcCode(DBBASE):
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, nullable=False)
     authcode = Column(Unicode(64), unique=True, nullable=False)
+    uri = Column(Unicode(256), nullable=False)
     expires_in = Column(Integer, nullable=False, default=10*60)
 
     revoked = Column(Boolean, default=False)
     revocation_date = Column(DateTime)
 
-    creation_date = Column(DateTime, default=datetime.utcnow)
+    creation_date = Column(DateTime, default=datetime.datetime.utcnow)
 
     client_id = Column(Integer, ForeignKey(OidcClient.id))
     client = relationship(OidcClient)
 
-    def __init__(self, client, user_id):
+    def __init__(self, client, user_id, uri):
         self.client = client
         self.user_id = user_id
+        self.uri = uri
 
         self.authcode = gen_token(self.client)
 
     def revoke(self):
+        """
+        Effectively set this instance as revoked
+        """
         self.revoked = True
-        self.revocation_date = datetime.utcnow()
+        self.revocation_date = datetime.datetime.utcnow()
 
-    def isRevoked(self):
-        expiry = time.mktime(self.create_date.timetuple()) + self.expires_in
-        if datetime.frometimestamp(expiry) < datetime.utcnow():
-            self.revoke()
+    def is_revoked(self):
+        """
+        Check if the code is revoked or not
+
+        :rtype: bool
+        """
+        if not self.revoked:
+            expiry = self.creation_date + datetime.timedelta(
+                minutes=self.expires_in
+            )
+            now = datetime.datetime.utcnow()
+            if expiry > now:
+                self.revoke()
+                DBSESSION().merge(self)
+
         return self.revoked
 
 
@@ -174,7 +252,7 @@ class OidcToken(DBBASE):
     revoked = Column(Boolean, default=False)
     revocation_date = Column(DateTime)
 
-    creation_date = Column(DateTime, default=datetime.utcnow)
+    creation_date = Column(DateTime, default=datetime.datetime.utcnow)
 
     client_id = Column(Integer, ForeignKey(OidcClient.id))
     client = relationship(OidcClient)
@@ -188,12 +266,22 @@ class OidcToken(DBBASE):
 
     def revoke(self):
         self.revoked = True
-        self.revocation_date = datetime.utcnow()
+        self.revocation_date = datetime.datetime.utcnow()
 
-    def isRevoked(self):
-        expiry = time.mktime(self.creation_date.timetuple()) + self.expires_in
-        if datetime.fromtimestamp(expiry) < datetime.utcnow():
-            self.revoke()
+    def is_revoked(self):
+        """
+        Check if the code is revoked or not
+
+        :rtype: bool
+        """
+        if not self.revoked:
+            expiry = self.creation_date + \
+                datetime.timedelta(minutes=self.expires_in)
+
+            now = datetime.datetime.utcnow()
+            if expiry > now:
+                self.revoke()
+                DBSESSION().merge(self)
         return self.revoked
 
     def refresh(self):
@@ -203,17 +291,83 @@ class OidcToken(DBBASE):
 
         cls = self.__class__
         self.revoke()
+        DBSESSION().merge(self)
         return cls(self.client, self.user_id)
 
-    def asJSON(self, **kwargs):
+    def __json__(self, request):
         token = {
+            'token_type': 'Bearer',
             'access_token': self.access_token,
             'refresh_token': self.refresh_token,
-            'user_id': self.user_id,
             'expires_in': self.expires_in,
         }
-        kwargs.update(token)
-        return kwargs
+        return token
+
+
+class OidcIdToken(DBBASE):
+    __table_args__ = default_table_args
+    id = Column(Integer, primary_key=True)
+    issuer = Column(Unicode(256), nullable=False)
+    sub = Column(Unicode(256), nullable=False)
+    expiration_time = Column(DateTime)
+    issue_time = Column(DateTime)
+
+    client_id = Column(Integer, ForeignKey(OidcClient.id))
+    client = relationship(OidcClient)
+
+    revoked = Column(Boolean, default=False)
+    revocation_date = Column(DateTime)
+
+    def __init__(self, issuer, client, code):
+        self.client = client
+        self.sub = code.user_id
+        self.issuer = issuer
+        self.issue_time = datetime.datetime.utcnow()
+        delta = datetime.timedelta(60)
+        self.expiration_time = self.issue_time + delta
+
+    def revoke(self):
+        self.revoked = True
+        self.revocation_date = datetime.datetime.utcnow()
+
+    def is_revoked(self):
+        if not self.revoked:
+            now = datetime.datetime.utcnow()
+            if self.expiration_time > now:
+                self.revoke()
+                DBSESSION().merge(self)
+        return self.revoked
+
+    @property
+    def aud(self):
+        return self.client.client_id
+
+    def __json__(self, request, claims=None):
+        result = {
+            'iss': self.issuer,
+            'sub': self.sub,
+            'aud': self.client_id,
+            'exp': dt_to_timestamp(self.expiration_time),
+            'iat': dt_to_timestamp(self.issue_time),
+        }
+        if claims is not None:
+            for key, value in claims.iteritems():
+                # We don't want to override the default openid scope datas
+                if key not in result:
+                    result[key] = value
+        return result
+
+    def __jwt__(self, request, claims):
+        """
+        Jwt tokens encoded with the client_secret as key
+
+        :param dict claims: Dict containing userdatas that should be added to
+        the current request (standard claims are described here :
+        http://openid.net/specs/openid-connect-core-1_0.html#StandardClaims )
+        """
+        json_datas = self.__json__(request, claims)
+        jwt_obj = jwt.encode(json_datas, self.client.cert_salt)
+        return jwt_obj
 
 
 def includeme(config):
